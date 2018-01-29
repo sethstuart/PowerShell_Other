87aacfa5e38f8a84719d1c88c14ddc34b61bb47b
Nick Hill <ndhill@gmail.com>
2018-01-29 12:53:54 -0700

    Update to Criteria and added PS Headder
    Changed criteria for excluding NICs to "*virtual*" from "*vmware*". Corrected variables used to confirm multiple active NICs before disabling the wireless. Added PS Header.

--- LAN_Switching.ps1
+++ LAN_Switching.ps1
@@ -1,6 +1,17 @@
+<#
+.SYNOPSIS
+Disable Wifi adapter when Ethernet is also connected.
+.DESCRIPTION
+Script checks if more than one active connection is present. If it finds more than one, it will disable the wireless connection. NOTE: Script ignores VMware 
+.FUNCTIONALITY
+Checkes for active physical ethernet connections. If it finds more than one, It will disable the wireless NIC.
+.EXAMPLE
+Scheduled task, or run manually.
+#>
+
 [boolean]$EthNICFound = $false
 [boolean]$wifiNICFound = $false
-$PhyConnectedNICs = Get-WmiObject -Class win32_networkadapter -computer . | where{($_.NetConnectionStatus -eq "2") -and ($_.Name -notLike "vmware*") -and ($_.Name -like "*Intel*")}
+$PhyConnectedNICs = Get-WmiObject -Class win32_networkadapter -computer . | Where-Object{($_.NetConnectionStatus -eq "2") -and ($_.Name -notLike "*virtual*") -and ($_.Name -like "*Intel(R)*")}
 
     if ($PhyConnectedNICs.count -ge "2")
     {
@@ -14,8 +25,8 @@ $PhyConnectedNICs = Get-WmiObject -Class win32_networkadapter -computer . | wher
 
             }
        }
-    if(($EthNIC) -and ($wifiNIC)){
-       $WifiNIC = Get-WmiObject -Class win32_networkadapter -computer . | where{($_.GUID -eq $wifiNIC.GUID)}
+    if(($EthNICFound) -and ($wifiNICFound)){
+       $WifiNIC = Get-WmiObject -Class win32_networkadapter -computer . | Where-Object{($_.GUID -eq $wifiNIC.GUID)}
         $wifiNIC.disable()     
     }
 }
