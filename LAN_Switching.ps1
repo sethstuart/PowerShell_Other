9670a6136b2cd7f7ea9f272ab3dbcf2854ae799a
Nick Hill <ndhill@gmail.com>
2018-02-02 17:20:14 -0700

    Added Progress Bars

--- LAN_Switching.ps1
+++ LAN_Switching.ps1
@@ -9,24 +9,44 @@ Checkes for active physical ethernet connections. If it finds more than one, It 
 Scheduled task, or run manually.
 #>
 
+
 [boolean]$EthNICFound = $false
 [boolean]$wifiNICFound = $false
+for($i = 1; $i -lt 101; $i++ ) {for($j=0;$j -lt 100;$j++) {} write-progress "Enumerating networtk connections." -perc $i;
+if(!$PhyConnectedNICs){
 $PhyConnectedNICs = Get-WmiObject -Class win32_networkadapter -computer . | Where-Object{($_.NetConnectionStatus -eq "2") -and ($_.Name -notLike "*virtual*") -and ($_.Name -like "*Intel(R)*")}
-
+}}
     if ($PhyConnectedNICs.count -ge "2")
     {
+        Write-Host "Identifying connected interfaces."
+        for($i = 15; $i -lt 101; $i++ ) {for($j=0;$j -lt 100;$j++) {} write-progress "Identifying wired and wirelss connections." "% Complete:" -perc $i;
+        if((!$EthNICFound) -and (!$EthNICFound)){
        $PhyConnectedNICs | %{
             if($_.NetConnectionID -eq "Local Area Connection"){
                 $EthNICFound = $true
+                Write-Host "Found Local Area Connection!"
+                Start-Sleep -Milliseconds 1200
             }
             if($_.NetConnectionID -eq "Wireless Network Connection"){
                 $wifiNICFound = $true
                 $wifiNIC = $_
-
+                Write-Host "Found Wireless Network Connection!"
+                Start-Sleep -Milliseconds 1200
             }
        }
+    }
+}
     if(($EthNICFound) -and ($wifiNICFound)){
+        for($i = 30; $i -lt 101; $i++ ) {for($j=0;$j -lt 100;$j++) {} write-progress "Disabling wireless connection." "% Complete:" -perc $i;
+        if(!$WifiOff){
+                
        $WifiNIC = Get-WmiObject -Class win32_networkadapter -computer . | Where-Object{($_.GUID -eq $wifiNIC.GUID)}
-        $wifiNIC.disable()     
+        
+       $WifiOff = $wifiNIC.disable()
+        }
+        }
+        if($WifiOff){
+            write-host "Done!"
+        }
     }
 }
